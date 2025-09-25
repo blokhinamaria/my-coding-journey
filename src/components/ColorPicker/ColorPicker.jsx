a9b2bc7f0b8ef0df63a9caf9afd285bb9c51f200
blokhinamaria <blokhinamariyayu@gmail.com>
2025-09-25 15:19:01 -0400

    Completed color picker

--- src/components/ColorPicker/ColorPicker.jsx
+++ src/components/ColorPicker/ColorPicker.jsx
@@ -6,43 +6,39 @@ export default function ColorPicker() {
 
     const baseURL = 'https://www.thecolorapi.com'
 
-    const defaultColors = [ '#000000', '#555555', '#CCCCCC', '#AAAAAA', '#FFFFFF'];
+    const defaultColors = [ '#F55A5A', '#2B283A', '#FBF3AB', '#AAD1B6', '#A626D3'];
 
     const [ colorsHex, setColorsHex] = useState(defaultColors);    
+    const [ selectedColorValue, setSelectedColorValue ] = useState('');
 
     function getColor(formData) {
         const seedColorHex = formData.get('seed-color').slice(1);
-        
         const colorScheme = formData.get('color-scheme');
 
         fetch(baseURL + `/scheme?hex=${seedColorHex}&mode=${colorScheme}&count=5`).then(res => res.json()).then(data => {
-            console.log()
+            console.log(data.colors)
             setColorsHex(data.colors.map(color => color.hex.value))
         });
-        document.getElementById('seed-color').setAttribute('defaultValue', `#${seedColorHex}`);
         document.querySelector(`[value=${colorScheme}]`).setAttribute('selected', 'selected');
     } 
 
     const colorBlocks = colorsHex.map(color => {
-        return (
-            <div key={color}>
-                <div  className='swatch' style={{backgroundColor:color}}></div>
-                <div className='code-block'>
-                    <p className='swatch-code'>{color}</p>
+            return (
+                <div key={color}>
+                    <div  className='swatch' style={{backgroundColor:color}}></div>
+                    <div className='code-block'>
+                        <p className='swatch-code'>{color}</p>
+                    </div>
                 </div>
-            </div>
-        )
-    }
-        
+            )
+        }
     )  
-
-    
 
     return (
         <main className="color-picker-main">
             <header>
-                <form action={getColor}>
-                    <input type="color" id="seed-color" name="seed-color" aria-label="seed-color" defaultValue="#F55A5A"
+                <form action={getColor} className='color-picker-form'>
+                    <input type="color" id="seed-color" name="seed-color" aria-label="seed-color" defaultValue={selectedColorValue} onChange={(e) => setSelectedColorValue(e.target.value)}
                     />
                     <select id='color-scheme' name='color-scheme'>
                         <option value='monochrome'>Monochrome</option>
@@ -54,13 +50,13 @@ export default function ColorPicker() {
                         <option value='triad'>Triad</option>
                         <option value='quad'>Quad</option>
                     </select>
-                    <button>Get color scheme</button>
+                    <button type='submit'>Get color scheme</button>
                 </form>
             </header>
-            <section id='colors'>
-                <div className='color-blocks'>
+            <section id='colors' className='color-blocks'>
+                
                     {colorBlocks}
-                </div>
+                
             </section>
         </main>
     )
